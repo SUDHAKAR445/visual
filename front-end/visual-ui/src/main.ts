00e74a4c12425a70b5a2e8a5c5413d25ce7966d7
Sudhakar445 <SUDHAKAR445>
2024-03-20 21:48:45 +0530

    update

--- front-end/visual-ui/src/main.ts
+++ front-end/visual-ui/src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { registerLicense } from '@syncfusion/ej2-base';
 import { AppModule } from './app/app.module';
 
-registerLicense("Ngo9BigBOggjHTQxAR8/V1NAaF1cXmhKYVF2WmFZfVpgdl9EYVZUTWYuP1ZhSXxXdkZiWn9ccnVQT2BbUEI=");
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCekx3THxbf1x0ZFRHallXTndeUiweQnxTdEFjWn1ecXVQT2FeV0B0Xg==");
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
